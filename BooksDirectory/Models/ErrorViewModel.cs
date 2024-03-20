ed56ab5be7382d8b1871f41aad8c2ac9fe61d4a7
Aleksandra Kuzeleva <a.brods@gmail.com>
2024-03-20 12:09:54 +0100

    commit

---
+++ BooksDirectory/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BooksDirectory.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
