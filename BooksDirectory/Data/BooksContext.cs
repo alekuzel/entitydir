37af9d897f4e83dae5bb6bd6bc875f5a455db7c0
Aleksandra Kuzeleva <a.brods@gmail.com>
2024-03-21 15:02:27 +0100

    commit

--- BooksDirectory/Data/BooksContext.cs
+++ BooksDirectory/Data/BooksContext.cs
@@ -12,3 +12,10 @@ namespace BooksDirectory.Data
         public DbSet<Book> Books { get; set; }
     }
 }
+
+
+//https://www.youtube.com/watch?v=jsAbuhGPT4Q
+//https://www.youtube.com/watch?v=ReAoBev65qI
+
+// models: Books, author, genre
+//Book: title, year, FK author, FK genre
--- BooksDirectory/obj/Debug/net8.0/BooksDirectory.AssemblyInfo.cs
+++ BooksDirectory/obj/Debug/net8.0/BooksDirectory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BooksDirectory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c541cc34196a8c8497c094255dae1a6fbc9eedf9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0f1e4168bf6013b5cadc808a050a36daacbe818")]
 [assembly: System.Reflection.AssemblyProductAttribute("BooksDirectory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BooksDirectory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
